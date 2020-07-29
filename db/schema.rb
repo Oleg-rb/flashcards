7f64d903231bafb169f11e617c4ae18f1e5e3ce2
Oleg-rb <oleg1107@rambler.ru>
2020-07-30 02:28:04 +0300

    Add model Card

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_07_29_181434) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cards", force: :cascade do |t|
+    t.text "original_text"
+    t.text "translated_text"
+    t.date "review_date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
