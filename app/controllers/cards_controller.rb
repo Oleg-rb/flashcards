32ad8cadb035241330cd7930379020e49007a19f
Oleg-rb <oleg1107@rambler.ru>
2020-08-08 21:34:25 +0300

    Add cards

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -2,7 +2,37 @@
 
 # Base controller of application
 class CardsController < ApplicationController
+
+  before_action :set_post, only: [ :show, :edit, :update, :destroy]
+
   def index
     @cards = Card.all
+  end
+
+  def new
+    @card = Card.new
+  end
+
+  def create
+    @card = Card.new card_params
+    if @card.save
+      redirect_to @card, success: 'Карточка успешно созданна'
+    else
+      flash.now[:danger] = 'Карточка не обновлена'
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  private
+
+  def set_post
+    @card = Card.find(params[:id])
+  end
+
+  def card_params
+    params.require(:card).permit(:original_text, :translated_text)
   end
 end
